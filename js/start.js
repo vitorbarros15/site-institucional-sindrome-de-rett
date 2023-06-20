58cdfe5b8351ce22da131863b4bd9f68b4ddf1ea
vitorbarros15 <vitorbarros15@hotmail.com>
2023-06-20 15:59:10 -0300

    Pagina Sobre Rett finalizada, partindo para desenvolvimento da pagina Abrete

--- js/start.js
+++ js/start.js
@@ -1,22 +1,162 @@
 
- // Função para carregar o conteúdo do arquivo HTML externo e inserir na divApp
- function carregarHTMLExterno() {
-    var divApp = document.getElementById('app');
-    
-    // Caminho para o arquivo HTML externo
-    var url = 'pages/home.html';
+/*Variáveis sub menu*/
+const btnVoltar = document.getElementById('btnVoltar');
+const navegationSobreRett = document.getElementById('navegationSobreRett');
+const subNavegation = document.getElementById('subNavegation');
 
-    fetch(url)
-        .then(response => response.text())
-        .then(data => {
-            divApp.innerHTML = data;
-        })
-        .catch(error => {
-            console.log('Erro ao carregar o arquivo HTML:', error);
-        });
+const homeLink = document.getElementById('home');
+const sobrerettLink = document.getElementById('sobrerett');
+const abreteLink = document.getElementById('abrete');
+const profissionaisLink = document.getElementById('profissionais');
+
+
+function limparSubMenu() {
+    navegationSobreRett.classList.add('hide')
+    navegationSobreRett.classList.remove('show')
+
+    btnVoltar.classList.add('hide')
+    btnVoltar.classList.remove('show')
+
+    subNavegation.classList.add('hide')
+    subNavegation.classList.remove('show')
 }
 
-// Chama a função para carregar o HTML externo quando a página é carregada
-document.addEventListener('DOMContentLoaded', function() {
-    carregarHTMLExterno();
+let routes = {};
+
+add('/', "pages/home.html")
+add("/sobrerett", "/pages/sobrerett.html")
+add("/abrete", "/pages/abrete.html")
+add(404, "/pages/404.html")
+add("/sobrerett/o-que-e-a-sindrome-de-rett/", "/pages/sobrerett/oQueSindromeRett.html")
+add("/sobrerett/diagnostico/", "/pages/sobrerett/diagnostico.html")
+add("/sobrerett/manistacoes/", "/pages/sobrerett/manistacoes.html")
+
+
+function add(routeName, page) {
+    routes[routeName] = page;
+}
+
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    function carregamentoConteudo(url, pushState = true) {
+        fetch(url).then(response => response.text()).then(html => {
+            document.getElementById('app').innerHTML = html;
+           
+            const btnCarregarHTMLOQueE = document.getElementById('btnOqueRett');
+            const btnCarregarHTMLDiagnostico = document.getElementById('btnDiagnostico');
+            const btnCarregarHTMLManifestacao = document.getElementById('btnManifestacoes');
+
+
+
+                btnCarregarHTMLOQueE.addEventListener('click', () => {
+                    fetch('pages/sobrerett/oQueSindromeRett.html').then(response => response.text()).then(html => {
+                        document.getElementById('app').innerHTML = html;
+                    })
+                    .catch(error => {
+                        console.error('Erro ao carregar o conteúdo de oQueSindromeRett.html:', error);
+                    });
+
+                    subNavegation.innerHTML = `<h1>- O que é a Sindrome de Rett?</h1>`;
+                    btnVoltar.classList.add('show');
+                    subNavegation.classList.add('show');
+                });
+
+
+                btnCarregarHTMLDiagnostico.addEventListener('click', () => {
+                    fetch('pages/sobrerett/diagnostico.html').then(response => response.text()).then(html => {document.getElementById('app').innerHTML = html;})
+                    .catch(error => {
+                        console.error('Erro ao carregar o conteúdo de diagnostico.html:', error);
+                    });
+
+                    subNavegation.innerHTML = `<h1>- Diagnóstico</h1>`;
+                    btnVoltar.classList.add('show')
+                    subNavegation.classList.add('show')
+                    
+                });
+
+                btnCarregarHTMLManifestacao.addEventListener('click', () => {
+                    fetch('pages/sobrerett/manifestacoes.html').then(response => response.text()).then(html => {document.getElementById('app').innerHTML = html;})
+                    .catch(error => {
+                        console.error('Erro ao carregar o conteúdo de manifestacao.html:', error);
+                    });
+
+                    subNavegation.innerHTML = `<h1>- Manifestações</h1>`;
+                    btnVoltar.classList.add('show')
+                    subNavegation.classList.add('show')
+                });
+
+
+                
+
+            if(url == 'pages/home.html') {
+                window.history.pushState({url}, '', '/');
+            }
+       
+        }).catch(error => {
+            console.log.error('Erro ao carregar o conteúdo:', error);
+        });
+    }
+
+    function handleNavigation(event) {
+        event = event || window.event
+        event.preventDefault();
+
+        window.history.pushState({}, "", event.target.href)
+
+        const route = event.target.getAttribute('href');
+        const url = routes[route] || routes[404]
+       
+        carregamentoConteudo(url)
+    }
+
+    document.getElementById('home').addEventListener('click', () => {
+        handleNavigation();
+        limparSubMenu();
+        homeLink.style.color = 'white';
+        sobrerettLink.style.color = 'black';
+        abreteLink.style.color = 'black';
+        profissionaisLink.style.color = 'black';
+    });
+
+    document.getElementById('sobrerett').addEventListener('click', () => {
+        handleNavigation();
+        limparSubMenu();
+        navegationSobreRett.classList.add('show');
+        homeLink.style.color = 'black';
+        sobrerettLink.style.color = 'white';
+        abreteLink.style.color = 'black';
+        profissionaisLink.style.color = 'black';
+    });
+
+    document.getElementById('abrete').addEventListener('click', () => {
+        limparSubMenu();
+        handleNavigation();
+
+        homeLink.style.color = 'black';
+        sobrerettLink.style.color = 'black';
+        abreteLink.style.color = 'white';
+        profissionaisLink.style.color = 'black';
+    });
+
+    document.getElementById('profissionais').addEventListener('click', () => {
+        limparSubMenu();
+        handleNavigation() 
+
+        homeLink.style.color = 'black';
+        sobrerettLink.style.color = 'black';
+        abreteLink.style.color = 'black';
+        profissionaisLink.style.color = 'white';
+    });
+
+    btnVoltar.addEventListener('click', function() {
+        carregamentoConteudo('pages/sobrerett.html')
+        limparSubMenu();
+        navegationSobreRett.classList.add('show');
+      });
+
+    carregamentoConteudo('pages/home.html');
 });
+
+
+  
